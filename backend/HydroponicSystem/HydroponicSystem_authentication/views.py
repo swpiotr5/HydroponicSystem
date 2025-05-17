e34d8cf9df52e3d24355a9fc30f1bd949ebe988d
Piotr Żywczak <123404938+zywczak@users.noreply.github.com>
2025-05-17 18:26:12 +0200

    create register user api view

--- backend/HydroponicSystem/HydroponicSystem_authentication/views.py
+++ backend/HydroponicSystem/HydroponicSystem_authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+class RegisterUserAPIView(APIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
