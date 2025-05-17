f54f924f6429e6ee53b3a0bd74fa50d1e4329546
Kondro571 <kondziot2000@gmail.com>
2025-05-17 19:36:31 +0200

    set urls

--- backend/HydroponicSystem/HydroponicSystem/urls.py
+++ backend/HydroponicSystem/HydroponicSystem/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from HydroponicSystem_systems.routers import router as systems_routers
+from HydroponicSystem_systems import urls as systems_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', include( systems_routers.urls)),
+    path('', include( systems_urls)),
 ]
